3c8485696750c329fc84000c05f58124c8b4fb34
seojin <jseojin0512@gmail.com>
2026-09-25 00:02:10 +0900

    feat: 메인페이지 ui 개선

--- my-react-app/src/components/movie.tsx
+++ my-react-app/src/components/movie.tsx
@@ -28,16 +28,18 @@ const Movie = () => {
   if (movie.length === 0) return <div>로딩 중...</div>;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {movie.map((movie) => (
-        <div key={movie.id} className="w-full">
-          <img src={movie.poster_path} alt={movie.original_title} />
-          <h3>{movie.original_title}</h3>
-          <p>평점: {movie.vote_average}</p>
-          <p>개봉일: {movie.release_date}</p>
-          
-        </div>
-      ))}
+    <div className="flex flex-col items-center">
+      <h1>Movie List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {movie.map((movie) => (
+          <div key={movie.id} className="w-full">
+            <img src={movie.poster_path} alt={movie.original_title} />
+            <h3>{movie.original_title}</h3>
+            <p>평점: {movie.vote_average}</p>
+            <p>개봉일: {movie.release_date}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- my-react-app/src/components/todolist.tsx
+++
@@ -1,9 +0,0 @@
-const AddTodo = () => {
-  return (
-    <>
-      <input style={{ border: "1px solid black" }} placeholder="할일 추가" />
-    </>
-  );
-};
-
-export default AddTodo;
